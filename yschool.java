683f6acdedd6cfbcda93c6f79b2aca5e04140837
Leesuhuck <globalmainpcb@gmail.com>
2020-10-29 00:12:24 +0900

    Add Java File [yschool.java]

---
+++ yschool.java
@@ -0,0 +1,23 @@
+public class yschool extends xschool {
+
+    public static void main (String args[]) {
+        xschool src = new xschool();
+        yschool brc = new yschool();
+
+    src.name="조던_피터슨";
+    src.student_ID="11-11111111";
+    src.address="USA";
+    src.age = "65";
+
+    brc.name="드웨인_존슨";
+    brc.student_ID="1234-12345678";
+    brc.address="USA";
+    brc.age = "42";
+    brc.clue ="희생";
+
+
+    }
+
+    
+
+}
