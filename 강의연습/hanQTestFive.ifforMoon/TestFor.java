04dad4658af75394b86d839aa3bf05f7c1ab2018
Leesuhuck <70427087+Leesuhuck@users.noreply.github.com>
2020-12-17 23:35:05 +0900

    Add files via upload

--- 강의연습/hanQTestFive.ifforMoon/TestFor.java
+++ 강의연습/hanQTestFive.ifforMoon/TestFor.java
@@ -4,9 +4,10 @@ public class TestFor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*
 		int i = 0,
 			j = 0;
-		
+		*/
 		// 후위 i++; 
 		// 전위 ++j; 
 		// 단항연산자 - 증감연산자 - 전위연산자, 후위연산자
@@ -17,7 +18,7 @@ public class TestFor {
 		
 		// int 	a = i++,
 		// 		b = ++j;
-
+		/*
 		 int 	a = i--,
 		 		b = --j;
 		
@@ -25,7 +26,45 @@ public class TestFor {
 		System.out.println(i);
 		
 		System.out.println(a+b);
+		*/
+		/*
+		for (int i = 0; i < 5; i++) {
+			//초기식, 조건식, 증감식
 		
+			 * 실행순서
+			 * 초기식
+			 * 조건식
+			 * 실행문
+			 * 증감식
+			 
+			sumM(i);
+		}
+		
+	}
+
+	public static void sumM(int valueM) {
+		
+		if (valueM==4&&valueM<5) {
+			System.out.println(valueM);
+		}else {
+			
+		}
+	}		
+	*/
+		
+		int a = 0;
+		
+		for (; a < 5; ++a) {
+
+			
+		}
+
+		sumM(a);
+	}
+	
+	public static void sumM(int valueM) {			
+			System.out.println(valueM);
+
 	}
 
 }
