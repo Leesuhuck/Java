c13b2211e1ff4b46f1b398471343e14c31b3e575
Leesuhuck <70427087+Leesuhuck@users.noreply.github.com>
2020-12-24 01:10:49 +0900

    Add files via upload

---
+++ 강의연습/hanQTestThree/DoPrint.java
@@ -0,0 +1,16 @@
+package hanQTestFour;
+
+import hanQTestSix.JavaFinal;
+
+public class DoPrint extends JavaFinal {
+	
+	public void consoleOut(String str) {
+		System.out.println(str);
+		
+		System.out.println(A);
+		System.out.println(B);
+		System.out.println(c);
+		
+		finalMethod();
+	}
+}
